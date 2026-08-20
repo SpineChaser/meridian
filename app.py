dfa8b4115a9f60e4491dcc5d7743ccfa710945ad
SpineChaser <gracewangariwaceke@gmail.com>
2026-08-20 17:38:02 +0300

    Add attendee state and duplicate protection

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+attendees = {}
 @app.route('/')
 def home():
     return "Meridian is running!"
@@ -8,6 +9,15 @@ def home():
 @app.route('/check-in', methods=['POST'])
 def check_in():
     data = request.get_json()
+    attendee_id = data.get('attendee_id')
+    if attendee_id in attendees:
+        return {
+            "message": "Attendee already checked in!",
+            "attendee": data
+        }, 409
+
+    attendees[attendee_id] = "checked_in"
+
     return {
         "message": "Check-in received!",
         "attendee": data
